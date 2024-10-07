9c65eb026577e00fd4c81880e96b6dc3d89909d8
Dominika Pirosova <pirosova.dominika@gmail.com>
2024-10-07 19:01:15 +0200

    modifikacia_4

--- script.js
+++ script.js
@@ -3,10 +3,8 @@
 const Prijem = (12 * 174 * 15)*30
 const Student = 0.35
 const Mesiac = 0.40
-
 const cenaStudenta = Math.floor((Prijem * Student) * Mesiac)
 
-document.body.innerHTML += "<p>Prijem" + "je" + Prijem " kc</p>"
-
-document.body.innerHTML += "<p>Cena" + " studenta" + " je" + cenaStudenta " kc</p>"
+document.body.innerHTML += "<p>Prijem" + "je" + Prijem + " kc</p>";
+document.body.innerHTML += "<p>Cena" + " studenta" + " je" + cenaStudenta + " kc</p>";
 
